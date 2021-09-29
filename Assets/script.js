32660a036839ffab08de7975e5ccc32a57f05c32
parisa-ostovari <parisaostovari@gmail.com>
2021-09-28 18:47:30 -0700

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercaseLettersArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var lowercaseLettersArray = 'abcdefghijklmnopqrstuvwxyz'
+var numbersArray = '0123456789'
+var specialCharactersArray = '!@#$%^&*()?{}[]-_+=,./'
+var passwordLength;
+var uppercaseLetters;
+var lowercaseLetters;
+var numbers;
+var specialCharacters;
+
+var characterSplit = numbersArray.split("")
+console.log(characterSplit)
+
+var characterSplit = uppercaseLettersArray.split("")
+console.log(characterSplit)
+
+var characterSplit = lowercaseLettersArray.split("")
+console.log(characterSplit)
+
+var characterSplit = specialCharactersArray.split("")
+console.log(characterSplit)
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +34,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
